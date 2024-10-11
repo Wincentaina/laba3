adc3478fbbdbf9ab2165100257e9a91a8cb98f79
Wincentaina <103582986+Wincentaina@users.noreply.github.com>
2024-10-11 20:22:50 +0700

    feat: добавлен класс TestCase

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
 
+using namespace std;
+
+class TestCase {
+private:
+    string input;    // Входные данные для теста
+    string expected; // Ожидаемый результат
+public:
+    TestCase(const string& input_str, const string& expected_str) // с помощью списка инициализации
+            : input(input_str), expected(expected_str) {}
+
+    const string& getInput() const { return input; }
+    const string& getExpected() const { return expected; }
+    // const указывает, что этот метод не изменяет состояние объекта.
+    // Это гарантирует, что метод может быть вызван даже для константных объектов.
+};
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    TestCase test1("input1", "expected1");
+    TestCase test2("input2", "expected2");
+
+    // Динамический массив объектов класса TestCase
+    TestCase* tests = new TestCase[2] {test1, test2};
 }
